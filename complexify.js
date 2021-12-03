58c6c14699610531acad9eaf9e914af45fdac14d
Unzor <71789103+Unzor@users.noreply.github.com>
2021-12-02 23:13:23 -0600

    Delete complexify.js

--- complexify.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-var uglify = require('uglify-js');
-var argOutPos = process.argv.indexOf('-o') + 1 | process.argv.indexOf('--output') + 1;
-var fName = process.argv.indexOf('-o') + 2 | process.argv.indexOf('--output') + 1;
-var fs = require('fs');
-var encoded = new TextEncoder().encode(fs.readFileSync(process.argv[fName]));
-var ets = encoded.toString().split(',');
-var new_file = uglify.minify(`((wrapper) => {wrapper.eval(new TextDecoder().decode(Uint8Array.from([${ets}])))})(global);`).code;
-fs.writeFileSync(process.argv[argOutPos], new_file);
